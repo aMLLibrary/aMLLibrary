4faae33c980ce701d23a61f2bdc0c2b369d8748a
hosseini <marjan.hosseini@polimi.it>
2018-12-25 22:31:52 +0100

    Ïmport from preparingData

--- etc/data_preprocessing.py
+++ etc/data_preprocessing.py
@@ -1,2 +1,25 @@
 
+import pandas as pd
+import numpy as np
+import math
+import ast
+import os
+import logging
+import configparser as cp
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import warnings
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings(action = 'ignore', category = DataConversionWarning)
+
+# Change str to float
+def is_column_line(str):
+    try:
+        float(str)
+        return False
+    except ValueError:
+        return True
+
 
