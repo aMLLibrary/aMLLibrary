6eff7df92f097a51a8906ba2f0908ea9d4b0f944
hosseini <marjan.hosseini@polimi.it>
2019-01-02 21:31:16 +0100

    Added combinatorial terms - 2 functions

--- etc/data_preprocessing.py
+++ etc/data_preprocessing.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 import numpy as np
+import itertools
+
 import math
 import ast
 import os
@@ -37,10 +39,69 @@ def is_column_line(str):
             print(feature_name)
             new_features_names.append(feature_name)
             new_features_dict[feature_name] = list(features_dict.get(feature_name))
-            invers_feature_name = 'inverse_'+feature_name
-            new_features_names.append(invers_feature_name)
+            inverse_feature_name = 'inverse_'+feature_name
+            new_features_names.append(inverse_feature_name)
             try:
-                new_features_dict[invers_feature_name] = list(1/np.array(new_features_dict[feature_name]))
+                new_features_dict[inverse_feature_name] = list(1/np.array(new_features_dict[feature_name]))
             except ValueError:
-                new_features_dict[invers_feature_name] = list(np.zeros(data_matrix.shape[0]))
+                new_features_dict[inverse_feature_name] = list(np.zeros(data_matrix.shape[0]))
         return new_features_dict
+
+    # Adds JUST the terms of the specified degree
+    def add_combinatorial_terms(data, degree):
+        new_features_dict = {}
+        data_matrix = pd.DataFrame.as_matrix(data)
+        features_names = list(data.columns.values)
+        indices = list(range(features_num))
+        combs = list(itertools.combinations_with_replacement(indices, degree))
+
+        for cc in combs:
+            temp = 1
+            for i in range(len(cc)):
+                    temp = np.multiply(temp,data_matrix[:, cc[i]])
+            new_feature_value = temp.reshape((data_matrix.shape[0], 1))
+            #new_feature_value=np.multiply(data_matrix[:, cc[0]],data_matrix[:, cc[1]])
+            data_matrix = np.append(data_matrix , new_feature_value, axis=1)
+            new_feature_name = ''
+            for i in range(len(cc)-1):
+                new_feature_name = new_feature_name+features_names[cc[i]]+'_'
+            new_feature_name = new_feature_name+features_names[cc[i+1]]
+            features_names.append(new_feature_name)
+            new_features_dict[new_feature_name] = list(new_feature_value.reshape(1,data_matrix.shape[0]))
+
+        #return features_names,data_matrix
+        return new_features_dict
+
+    # Adds ALL the terms up to the specified degree
+    def add_all_comb(data,degree):
+        new_features_dict = {}
+        data_matrix = pd.DataFrame.as_matrix(data)
+        features_names = list(data.columns.values)
+        indices = list(range(features_num))
+
+        for j in range(2, degree + 1):
+            combs = list(itertools.combinations_with_replacement(indices, j))
+
+            for cc in combs:
+                temp = 1
+                for i in range(len(cc)):
+                        temp = np.multiply(temp,data_matrix[:, cc[i]])
+                new_feature_value = temp.reshape((data_matrix.shape[0], 1))
+                #new_feature_value=np.multiply(data_matrix[:, cc[0]],data_matrix[:, cc[1]])
+                data_matrix = np.append(data_matrix , new_feature_value, axis=1)
+                new_feature_name = ''
+                for i in range(len(cc)-1):
+                    new_feature_name = new_feature_name+features_names[cc[i]]+'_'
+                new_feature_name = new_feature_name+features_names[cc[i+1]]
+                features_names.append(new_feature_name)
+                new_features_dict[new_feature_name] = list(new_feature_value.reshape(1,data_matrix.shape[0]))
+
+            #return features_names,data_matrix
+            return new_features_dict
+
+
+
+
+
+
+
