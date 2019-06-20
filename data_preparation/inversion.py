Fixed normalization

data_preparation/inversion.py
@@ -47,13 +47,13 @@ class Inversion(data_preparation.data_preparation.DataPreparation):
 
         to_be_inv_list = self._campaign_configuration['DataPreparation']['inverse']
         if to_be_inv_list == "[*]":
-            to_be_inv_list = self._campaign_configuration['Features']['Extended_feature_names'].copy()
+            to_be_inv_list = inputs.x_columns.copy()
 
         for column in to_be_inv_list:
-            new_column = 1 / np.array(inputs[column])
+            new_column = 1 / np.array(inputs.data[column])
             new_feature_name = 'inverse_' + column
-            outputs[new_feature_name] = new_column
-            self._campaign_configuration['Features']['Extended_feature_names'].append(new_feature_name)
+            outputs.data[new_feature_name] = new_column
+            outputs.x_columns.append(new_feature_name)
 
         return outputs
 

data_preparation/product.py
@@ -52,9 +52,9 @@ class Product(data_preparation.data_preparation.DataPreparation):
 
         max_degree = self._campaign_configuration["DataPreparation"]['product_max_degree']
         if str(max_degree) == "inf":
-            max_degree = len(self._campaign_configuration['Features']['Extended_feature_names'])
+            max_degree = len(inputs.x_columns)
 
-        features = sorted(set(self._campaign_configuration['Features']['Extended_feature_names']))
+        features = sorted(set(inputs.x_columns))
 
         for degree in range(2, max_degree + 1):
             combinations = itertools.combinations(features, degree)
@@ -64,10 +64,10 @@ class Product(data_preparation.data_preparation.DataPreparation):
                 #Compute the string for combination[:-2]
                 print(combination)
                 base = self._compute_column_name(combination[:-1])
-                new_column = np.array(outputs[base]) * np.array(outputs[combination[-1]])
+                new_column = np.array(outputs.data[base]) * np.array(outputs.data[combination[-1]])
                 new_feature_name = self._compute_column_name(combination)
-                outputs[new_feature_name] = new_column
-                self._campaign_configuration['Features']['Extended_feature_names'].append(new_feature_name)
+                outputs.data[new_feature_name] = new_column
+                outputs.x_columns.append(new_feature_name)
 
         return outputs
 

model_building/experiment_configuration.py
@@ -129,8 +129,8 @@ class ExperimentConfiguration(abc.ABC):
         self._logger.debug("Validating model")
         predicted_y = self.compute_estimations(validation_rows)
         real_y = self._regression_inputs.data.loc[validation_rows, self._regression_inputs.y_column].values.astype(np.float64)
-        if self._regression_inputs.y_scaler:
-            y_scaler = self._regression_inputs.y_scaler
+        if self._regression_inputs.y_column in self._regression_inputs.scalers:
+            y_scaler = self._regression_inputs.scalers[self._regression_inputs.y_column]
             predicted_y = y_scaler.inverse_transform(predicted_y)
             real_y = y_scaler.inverse_transform(real_y)
         difference = real_y - predicted_y