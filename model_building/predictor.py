50772c421c354964b576d8215683c548b953ee16
brunoguindani <bruno@guindani.org>
2021-06-21 22:47:35 +0200

    Better prediction files

--- model_building/predictor.py
+++ model_building/predictor.py
@@ -1,36 +1,91 @@
-from configparser import ConfigParser
-import pandas as pd
+import ast
+import configparser as cp
+import logging
+import os
 import pickle
+import pprint
+import sys
 import numpy as np
+import pandas as pd
 
+import custom_logger
 import data_preparation.data_loading
 import data_preparation.onehot_encoding
+import model_building.model_building
+
 
 class Predictor:
-  def __init__(self, config_file, regressor_file, output_file):
-    # Initialize strings
-    self._config_file = config_file
-    self._regressor_file = regressor_file
-    self._output_file = output_file
-    print(f"Parameters: {self._config_file}, {self._regressor_file}, and "
-          f"{self._output_file}")
+    def __init__(self, config_file, regressor_file, output_folder):
+        logging.basicConfig(level=logging.INFO)
+        self._logger = custom_logger.getLogger(__name__)
+        self._output_folder = output_folder
+        self._regressor_file = regressor_file
 
-    # Read data
-    config = ConfigParser()
-    config.read(config_file)
-    self._data = pd.read_csv(config['DataPreparation']['input_path'])
-    self._xx = self._data.drop(columns=[config['General']['y']])
-    self._yy = self._data[config['General']['y']]
+        # Read config file
+        self.conf = cp.ConfigParser()
+        self.conf.optionxform = str
+        self.conf.read(config_file)
+        self._campaign_configuration = {}
+        self.load_campaign_configuration()
 
-    # Read regressor
-    with open(regressor_file, "rb") as f:
-      self._regressor = pickle.load(f)
+        # Check if output path already exist
+        if os.path.exists(output_folder):
+            self._logger.error("%s already exists", output_folder)
+            sys.exit(1)
+        os.mkdir(output_folder)
 
-  def predict(self):
-    self._yy_predicted = self._regressor.predict(self._xx)
-    yy_both = pd.DataFrame()
-    yy_both['real'] = self._yy
-    yy_both['pred'] = self._yy_predicted
-    print(yy_both)
-    mape = np.mean(np.abs(np.divide(self._yy-self._yy_predicted, self._yy)))
-    print("MAPE =", str(mape))
+        # Read data
+        self._logger.info("-->Executing data load")
+        data_loader = data_preparation.data_loading.DataLoading(self._campaign_configuration)
+        self._datb = data_loader.process(None)
+        self._logger.info("Current data frame is:\n%s", str(self._datb))
+        self._logger.info("<--")
+        self._datb = self._datb.data
+        self._logger.info("Current data frame is:\n%s", str(self._datb))
+        self._logger.info("<--")
+
+
+    def load_campaign_configuration(self):
+        self._campaign_configuration = {}
+
+        for section in self.conf.sections():
+            self._campaign_configuration[section] = {}
+            for item in self.conf.items(section):
+                try:
+                    self._campaign_configuration[section][item[0]] = ast.literal_eval(item[1])
+                except (ValueError, SyntaxError):
+                    self._campaign_configuration[section][item[0]] = item[1]
+
+        self._logger.info("Parameters configuration is:")
+        self._logger.info("-->")
+        self._logger.info(pprint.pformat(self._campaign_configuration, width=1))
+        self._logger.info("<--")
+
+
+    def predict(self):
+        self._logger.info("-->Executing prediction")
+        yy = self._datb[self._campaign_configuration['General']['y']]
+        self._datb = self._datb.drop(columns=[self._campaign_configuration['General']['y']])
+        with open(self._regressor_file, "rb") as f:
+            regressor = pickle.load(f)
+        yy_pred = regressor.predict(self._datb)
+
+        # Write predictions to file
+        yy_both = pd.DataFrame()
+        yy_both['real'] = yy
+        yy_both['pred'] = yy_pred
+        yy_file = os.path.join(self._output_folder, 'prediction.csv')
+        with open(yy_file, 'w') as f:
+            yy_both.to_csv(f, index=False)
+        self._logger.info("Saved to %s", str(yy_file))
+
+        # Compute MAPE
+        difference = yy - yy_pred
+        mape = np.mean(np.abs(np.divide(difference, yy)))
+        self._logger.info("---MAPE = %s", str(mape))
+        mape_file = os.path.join(self._output_folder, 'mape.txt')
+        with open(mape_file, 'w') as f:
+          f.write(str(mape))
+          f.write('\n')
+
+        self._logger.info("<--Performed prediction")
--- predict.py
+++ predict.py
@@ -6,17 +6,15 @@ from model_building.predictor import Predictor
 def main():
     parser = argparse.ArgumentParser(
       description="Perform prediction on new data using the provided models")
-    parser.add_argument('-c', "--config-file",
-      help="configuration file for the infrastructure", required=True)
-    parser.add_argument('-r', "--regressor",
-      help="binary regressor file to be used", required=True)
-    parser.add_argument('-o', "--output",
-      help="output file where predictions will be stored", required=True)
+    parser.add_argument('-c', "--config-file", help="configuration file for the infrastructure", required=True)
+    parser.add_argument('-r', "--regressor", help="binary regressor file to be used", required=True)
+    parser.add_argument('-o', "--output", help="output folder where predictions will be stored", required=True)
+    #parser.add_argument('-d', "--debug", help="Enable debug messages", default=False, action="store_true")
     args = parser.parse_args()
 
     predictor_obj = Predictor(config_file=args.config_file,
                               regressor_file=args.regressor,
-                              output_file=args.output)
+                              output_folder=args.output)
     predictor_obj.predict()
 
 if __name__ == '__main__':
