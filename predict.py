529ff1ff672acd21d68cb187490402f8b80aa6b0
brunoguindani <bruno@guindani.org>
2021-06-25 15:31:37 +0200

    Updated CL params

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Perform prediction on new data using the provided models")
     parser.add_argument("-c", "--config-file",  help="configuration file for the infrastructure", required=True)
     parser.add_argument("-r", "--regressor",    help="binary regressor file to be used", required=True)
-    parser.add_argument("-o", "--output",       help="output folder where predictions will be stored", required=True)
+    parser.add_argument("-o", "--output",       help="output folder where predictions will be stored", default='output_predict')
     parser.add_argument("-d", "--debug",        help="Enable debug messages", default=False, action='store_true')
     parser.add_argument("-m", "--mape-to-text", help="Write MAPE to text file", default=False, action='store_true')
     args = parser.parse_args()
@@ -22,4 +22,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Example: pipenv run python predict.py -c example_configurations/00_predict.ini -r output_agora/blockscholes/blockscholes_itr1/LRRidge.pickle -o output_predict
+# Example: python predict.py -m -c example_configurations/faas_predict.ini -r output_faas2/LRRidge.pickle -o output_faas2_predict
