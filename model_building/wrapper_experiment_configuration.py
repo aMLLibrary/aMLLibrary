937c9e607ccacb2e999d82b5ef2a83b76671dcda
brunoguindani <bruno@guindani.org>
2021-09-13 09:57:09 +0200

    Moved some code into new _check_num_features()

--- model_building/wrapper_experiment_configuration.py
+++ model_building/wrapper_experiment_configuration.py
@@ -157,8 +157,13 @@ class SFSExperimentConfiguration(WrapperExperimentConfiguration):
 
         """
         super().__init__(campaign_configuration, regression_inputs, prefix, wrapped_experiment_configuration)
+        self._check_num_features()
+
+    def _check_num_features(self):
+        """
+        Exit if the maximum number of required features is greater than the number of existing features
+        """
         temp_xdata, temp_ydata = self._regression_inputs.get_xy_data(self._regression_inputs.inputs_split["training"])
-        # if the maximum number of required features is greater than the number of existing features, exit
         if self._campaign_configuration['FeatureSelection']['max_features'] > temp_xdata.shape[1]:
             self._logger.error("ERROR: The maximum number of required features must be in range(1, %d)", temp_xdata.shape[1]+1)
             sys.exit(-10)
@@ -384,11 +389,7 @@ class HyperoptSFSExperimentConfiguration(HyperoptExperimentConfiguration):
         TODO
         """
         super().__init__(campaign_configuration, regression_inputs, prefix, wrapped_experiment_configuration)
-        # Check if the maximum number of required features is greater than the number of existing features
-        temp_xdata, temp_ydata = self._regression_inputs.get_xy_data(self._regression_inputs.inputs_split["training"])
-        if self._campaign_configuration['FeatureSelection']['max_features'] > temp_xdata.shape[1]:
-            self._logger.error("ERROR: The maximum number of required features must be in range(1, %d)", temp_xdata.shape[1]+1)
-            sys.exit(-10)
+        SFSExperimentConfiguration._check_num_features(self)
 
     def _get_standard_evaluator(self, scorer):
         """
