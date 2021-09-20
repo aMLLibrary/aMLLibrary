f69b5438d2e65de140b49889bf8ca213a31397b8
brunoguindani <bruno@guindani.org>
2021-09-20 10:50:50 +0200

    Added GPU-Data stuff

---
+++ myscripts/run_gpudata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_OUTPUT_FOLD=outputs/output_gpudata_08
+mkdir $BASE_OUTPUT_FOLD
+
+for config in example_configurations/gpudata/*.ini; do
+  CONFIG_NO_EXT="${config%.*}"
+  OUTPUT_FOLD=$BASE_OUTPUT_FOLD/$(basename $CONFIG_NO_EXT)
+  echo pipenv run python ./run.py -c $config -o $OUTPUT_FOLD
+  #pipenv run python ./run.py -c $config -o $OUTPUT_FOLD
+done
