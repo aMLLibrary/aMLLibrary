320f05e35bd2fd1ab4185f920d0beb3d13636304
brunoguindani <bruno@guindani.org>
2021-09-24 17:27:42 +0200

    Updated scripts

--- myscripts/run_extrap_N_gpudata.sh
+++ myscripts/run_extrap_N_gpudata.sh
@@ -7,5 +7,5 @@ for config in example_configurations/gpudata/extrapolation_N/*.ini; do
   CONFIG_NO_EXT="${config%.*}"
   OUTPUT_FOLD=$BASE_OUTPUT_FOLD/$(basename $CONFIG_NO_EXT)
   echo pipenv run python ./run.py -c $config -o $OUTPUT_FOLD
-  pipenv run python ./run.py -c $config -o $OUTPUT_FOLD
+  #pipenv run python ./run.py -c $config -o $OUTPUT_FOLD
 done
