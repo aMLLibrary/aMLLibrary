28de77ecd111fa83684cc5e04233118874ab6331
brunoguindani <bruno@guindani.org>
2021-10-18 17:29:18 +0200

    Deleted old scripts

--- myscripts/amltesthp.sh
+++
@@ -1,7 +0,0 @@
-rm -rf outputs/output_faas_test*
-pipenv run python ./run.py -c example_configurations/faas_test_hyperopt.ini \
-       -o outputs/output_faas_test_3_hp
-echo; echo; echo; echo
-pipenv run python ./run.py -c example_configurations/faas_test_hyperopt_sfs.ini \
-       -o outputs/output_faas_test_4_hp_sfs
-chmod -R a+rw outputs/output_faas_test*
