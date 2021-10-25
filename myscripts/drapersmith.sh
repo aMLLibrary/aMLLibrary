eafef7d3ecb5dfc9e47b97a0c0ecba2e7df3e8e2
brunoguindani <bruno@guindani.org>
2021-10-25 18:06:19 +0200

    Updated drapersmith.sh

--- myscripts/drapersmith.sh
+++ myscripts/drapersmith.sh
@@ -4,7 +4,7 @@ BASE_OUTPUT_FOLD="outputs/gpudata/drapersmith"
 rm -rf $BASE_OUTPUT_FOLD
 mkdir  $BASE_OUTPUT_FOLD
 
-for conf in example_configurations/gpudata/stepwise_extrapolation_P/*/resnet50_pt.ini; do
+for conf in example_configurations/gpudata/stepwise_extrapolation_N/*/*.ini; do
   NAME=$(basename $conf)
   NAME="${NAME%.*}"
   OUTPUT_FOLD=$BASE_OUTPUT_FOLD/$NAME
