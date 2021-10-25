b4d684d04d40176857e0448e4c54913552a05cff
brunoguindani <bruno@guindani.org>
2021-10-25 09:35:54 +0200

    Added script to analyze problematic Draper-Smith cases

---
+++ myscripts/drapersmith.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASE_OUTPUT_FOLD="outputs/gpudata/drapersmith"
+rm -rf $BASE_OUTPUT_FOLD
+mkdir  $BASE_OUTPUT_FOLD
+
+for conf in example_configurations/gpudata/stepwise_extrapolation_P/stepwise/*.ini; do
+  NAME=$(basename $conf)
+  NAME="${NAME%.*}"
+  OUTPUT_FOLD=$BASE_OUTPUT_FOLD/$NAME
+  echo python3 ./run.py -c $conf -o $OUTPUT_FOLD
+  python3 ./run.py -c $conf -o $OUTPUT_FOLD
+done
