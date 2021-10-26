9b0c8887009b1b35e251c90c553a768a07fa6181
brunoguindani <bruno@guindani.org>
2021-10-26 12:08:31 +0200

    Updated drapersmith.sh

--- myscripts/drapersmith.sh
+++ myscripts/drapersmith.sh
@@ -8,6 +8,6 @@ for conf in example_configurations/gpudata/stepwise_extrapolation_N/*/*.ini; do
   NAME=$(basename $conf)
   NAME="${NAME%.*}"
   OUTPUT_FOLD=$BASE_OUTPUT_FOLD/$NAME
-  echo python3 ./run.py -c $conf -o $OUTPUT_FOLD
-  python3 ./run.py -c $conf -o $OUTPUT_FOLD
+  echo python3 ./run.py -c $conf -o $OUTPUT_FOLD $@
+  python3 ./run.py -c $conf -o $OUTPUT_FOLD $@
 done
