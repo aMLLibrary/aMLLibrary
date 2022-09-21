ce69f0483a5f0bbb56b8f0fd82fdc88a90a279f3
nahuel-coliva <nahuel.coliva@polimi.it>
2022-09-21 17:15:26 +0200

    added missing return

--- model_building/model_building.py
+++ model_building/model_building.py
@@ -74,10 +74,10 @@ class ModelBuilding:
         """
         if self.debug:
             # Do not use try-except mechanism
-            experiment_configuration.train()#force=False,disable_model_parallelism=True)
+            experiment_configuration.train(disable_model_parallelism=True)
         else:
             try:
-                experiment_configuration.train()#force=False,disable_model_parallelism=True)
+                experiment_configuration.train(disable_model_parallelism=True)
             except KeyError as ki:
                 if hasattr(experiment_configuration, '_sfs') and experiment_configuration._sfs.interrupted_:
                     raise KeyboardInterrupt
@@ -86,6 +86,7 @@ class ModelBuilding:
                 raise key
             except Exception as e:
                 self._logger.debug(str(e))
+        return experiment_configuration
 
     def process(self, campaign_configuration, regression_inputs, processes_number):
         """
@@ -226,7 +227,7 @@ class ModelBuilding:
             best_conf.set_training_data(all_data)
 
             # Train and evaluate by several metrics
-            best_conf.train(force=True)#,disable_model_parallelism=False)
+            best_conf.train(force=True)
             best_conf.evaluate()
 
             self._logger.debug("After final training of "+str(technique)+", features look like:")
